180acd26de28cb8634ef77d4e248214eeb409d1f
s.sim <s.sim@esrisingapore.com.sg>
2024-07-02 13:15:58 +0800

    create button list

--- src/App.js
+++ src/App.js
@@ -4,9 +4,84 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <a href="https://samsonsim2-default-thelaylaway.dev.8thwall.app/samson-test/">
-        Link
-      </a>
+      <ul
+        style={{
+          display: "flex",
+          listStyleType: "none",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "1rem",
+        }}
+      >
+        <li>
+          <div
+            style={{
+              backgroundColor: "purple",
+              width: "100px",
+              padding: "10px",
+              borderRadius: "20px",
+            }}
+          >
+            <a
+              style={{ color: "white", textDecoration: "none" }}
+              href="https://samsonsim2-default-thelaylaway.dev.8thwall.app/samson-test/"
+            >
+              Filter 1
+            </a>
+          </div>
+        </li>
+        <li>
+          <div
+            style={{
+              backgroundColor: "purple",
+              width: "100px",
+              padding: "10px",
+              borderRadius: "20px",
+            }}
+          >
+            <a
+              style={{ color: "white", textDecoration: "none" }}
+              href="https://samsonsim2-default-thelaylaway.dev.8thwall.app/samson-test/"
+            >
+              Filter 2
+            </a>
+          </div>
+        </li>
+        <li>
+          <div
+            style={{
+              backgroundColor: "purple",
+              width: "100px",
+              padding: "10px",
+              borderRadius: "20px",
+            }}
+          >
+            <a
+              style={{ color: "white", textDecoration: "none" }}
+              href="https://samsonsim2-default-thelaylaway.dev.8thwall.app/samson-test/"
+            >
+              Filter 3
+            </a>
+          </div>
+        </li>
+        <li>
+          <div
+            style={{
+              backgroundColor: "purple",
+              width: "100px",
+              padding: "10px",
+              borderRadius: "20px",
+            }}
+          >
+            <a
+              style={{ color: "white", textDecoration: "none" }}
+              href="https://samsonsim2-default-thelaylaway.dev.8thwall.app/samson-test/"
+            >
+              Filter 4
+            </a>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
